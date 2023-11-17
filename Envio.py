a74ec7e01fa54be21adddf7206e21e25e459e206
Gestora Tech | Repositório <114442086+gestoratech@users.noreply.github.com>
2023-11-17 11:04:43 -0300

    Envio.py

--- Envio.py
+++ Envio.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
